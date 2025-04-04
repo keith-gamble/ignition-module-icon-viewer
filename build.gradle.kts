8b7e1bcb309a8b8f5184d66a6a16b9f85fdc60ab
Paul Griffith <pgriffith@ia.io>
2025-04-04 08:21:59 -0700

    Remove unnecessary reflection

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,23 @@ plugins {
 }
 
 repositories {
-    mavenLocal()
     maven {
         url = uri("https://nexus.inductiveautomation.com/repository/public/")
     }
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 dependencies {
-    implementation("com.inductiveautomation.ignitionsdk:ignition-common:8.1.24")
-    implementation("com.inductiveautomation.ignitionsdk:client-api:8.1.24")
+    val version = "8.1.24"
+    implementation(group = "com.inductiveautomation.ignitionsdk", name = "ignition-common", version = version)
+    implementation(group = "com.inductiveautomation.ignitionsdk", name = "client-api", version = version)
 }
 
 application {
-    mainClass.set("dev.kgamble.IconViewer")
+    mainClass.set("com.inductiveautomation.ignition.client.icons.VectorIcons")
 }
